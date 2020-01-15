1e918aca230933ba7d12809114d8c12e02383c8c
LEP5KOR <NagaSaiNarayanaKartheek.Palepu@in.bosch.com>
2020-01-15 14:19:45 +0530

    Added nltk download at beginning.

--- src/user.py
+++ src/user.py
@@ -5,10 +5,15 @@ import warnings
 import numpy as np
 import pandas as pd
 import tldextract
-from nltk.corpus import stopwords
+import nltk
+
+nltk.download('stopwords')
+
+# from nltk.corpus import stopwords # Removed it to fit requirements better
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk.util import ngrams
+
 from scipy import stats
 
 from action_logging import Logger
@@ -392,7 +397,24 @@ class User:
 
         :return:
         """
-        words = [word for word in words if word not in stopwords.words('english')]
+        stopwords_list = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've",
+                          "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself',
+                          'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them',
+                          'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll",
+                          'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has',
+                          'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or',
+                          'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against',
+                          'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from',
+                          'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once',
+                          'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
+                          'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than',
+                          'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now',
+                          'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn',
+                          "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn',
+                          "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't",
+                          'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn',
+                          "wouldn't"]
+        words = [word for word in words if word not in stopwords_list]
         words = [word for word in words if word not in ['haan', 'okay', 'hahaha', 'tc', 'sd']]
         return words
 
@@ -725,10 +747,12 @@ class User:
             create_response_time()
         if self.user_name != 'Overall':
             response_time = response_obj.grouped_data[
-                (response_obj.grouped_data['User'] == self.user_name) & (response_obj.grouped_data['Response (Min)'] > 0)][
+                (response_obj.grouped_data['User'] == self.user_name) & (
+                        response_obj.grouped_data['Response (Min)'] > 0)][
                 'Response (Min)']
         else:
-            response_time = response_obj.grouped_data[(response_obj.grouped_data['Response (Min)'] > 0)]['Response (Min)']
+            response_time = response_obj.grouped_data[(response_obj.grouped_data['Response (Min)'] > 0)][
+                'Response (Min)']
         self.avg_response_time = np.round(stats.hmean(response_time), 3)
         self.logger.write_logger(f'In user.py (get_response_time): Fetching response time ends')
         return self
